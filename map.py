a628fc4009d0e433490e10487c375eb86957e02e
RatherChaotic <OccationallyChaotic@outlook.com>
2023-12-04 08:10:44 -0500

    Updated map.py to support collisions

--- map.py
+++ map.py
@@ -4,6 +4,8 @@
 import pygame as pg
 import pytmx as tmx
 import pyscroll as ps
+
+
 class Map(object):
     def __init__(self):
         self.frame = None
@@ -14,13 +16,23 @@ class Map(object):
     def load(self, filename):
         self.tmx_data = tmx.load_pygame(filename)
         self.map_data = ps.data.TiledMapData(self.tmx_data)
-        self.map_layer = ps.BufferedRenderer(self.map_data, (pg.display.get_surface().get_width(), pg.display.get_surface().get_height()), clamp_camera=True)
+        self.map_layer = ps.BufferedRenderer(self.map_data, (
+        pg.display.get_surface().get_width(), pg.display.get_surface().get_height()), clamp_camera=True)
         self.group = ps.PyscrollGroup(map_layer=self.map_layer, default_layer=4)
 
-    def draw(self):
-        self.group.center(pg.mouse.get_pos())
+    def draw(self, center):
+        self.group.center(center)
         self.group.draw(pg.display.get_surface())
-        
 
-    def update(self):
-        self.draw()
+    def collide(self, player):
+        for layer in self.tmx_data.visible_layers:
+            if isinstance(layer, tmx.TiledObjectGroup):
+                if layer.name == "collision":
+                    for obj in layer:
+                        rect = pg.Rect(obj.x, obj.y, obj.width, obj.height)
+                        if rect.colliderect(player.rect):
+                            return True
+
+    def update(self, center, player):
+        self.draw(center)
+        self.collide(player)
