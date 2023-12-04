407ab6b6c909fa5f26026939c1bd03c1f11fa86a
RatherChaotic <OccationallyChaotic@outlook.com>
2023-12-04 08:38:09 -0500

    Addded misc.py and player.py

--- map.py
+++ map.py
@@ -6,6 +6,7 @@ import pytmx as tmx
 import pyscroll as ps
 
 
+# Initialize Class
 class Map(object):
     def __init__(self):
         self.frame = None
@@ -13,17 +14,20 @@ class Map(object):
         self.map_data = None
         self.tmx_data = None
 
+    # Load the map files and initialize the pyscroller
     def load(self, filename):
         self.tmx_data = tmx.load_pygame(filename)
         self.map_data = ps.data.TiledMapData(self.tmx_data)
         self.map_layer = ps.BufferedRenderer(self.map_data, (
-        pg.display.get_surface().get_width(), pg.display.get_surface().get_height()), clamp_camera=True)
+            pg.display.get_surface().get_width(), pg.display.get_surface().get_height()), clamp_camera=True)
         self.group = ps.PyscrollGroup(map_layer=self.map_layer, default_layer=4)
 
+    # Draw the map
     def draw(self, center):
         self.group.center(center)
         self.group.draw(pg.display.get_surface())
 
+    # Check for collisions
     def collide(self, player):
         for layer in self.tmx_data.visible_layers:
             if isinstance(layer, tmx.TiledObjectGroup):
---
+++ player.py
@@ -0,0 +1,10 @@
+# Player Loading File
+# Created by https://github.com/RatherChaotic
+
+import pygame as pg
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        
