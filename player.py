d1cf7ae778eed109a093b138f74f36a3087756f8
RatherChaotic <OccationallyChaotic@outlook.com>
2023-12-05 08:30:21 -0500

    Trying to unbugify the collision

--- player.py
+++ player.py
@@ -9,23 +9,28 @@ class Player(pg.sprite.Sprite):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
-        self.old_pos = (0, 0)
+        self.old_pos = [0, 0]
 
-    def handle_movement(self):
+    def handle_movement(self, map):
         keys = pg.key.get_pressed()
-        if keys[pg.K_a]:
+        if keys[pg.K_a] and not map.collide(self):
             self.rect.centerx -= 5
-        elif keys[pg.K_d]:
+        elif keys[pg.K_d] and not map.collide(self):
             self.rect.centerx += 5
-        if keys[pg.K_w]:
+        else:
+            self.move_back("x")
+        if keys[pg.K_w] and not map.collide(self):
             self.rect.centery -= 5
-        elif keys[pg.K_s]:
+        elif keys[pg.K_s] and not map.collide(self):
             self.rect.centery += 5
-
+        elif map.collide(self):
+            self.move_back("y")
     def update(self, map):
         self.old_pos = self.rect.x, self.rect.y
-        self.handle_movement()
-        map.collide(self)
+        self.handle_movement(map)
 
-    def move_back(self):
-        self.rect.x, self.rect.y = self.old_pos
+    def move_back(self, mode):
+        if mode == "x":
+            self.rect.x = self.old_pos[0]
+        elif mode == "y":
+            self.rect.y = self.old_pos[1]
